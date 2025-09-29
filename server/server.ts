15f53775e0098fed13b235ff206469bfb417c3de
Abe <214738266+abe-calder@users.noreply.github.com>
2025-09-29 16:08:16 +1300

    add dotenv new import to server.ts

--- server/server.ts
+++ server/server.ts
@@ -2,11 +2,18 @@ import { join } from 'node:path'
 import express from 'express'
 import * as Path from 'node:path'
 import * as URL from 'node:url'
-
 import clash from './routes/clash.ts'
 import clashBattleLog from './routes/clashBattlelog.ts'
 import clashLeaderboard from './routes/clashLeaderboard.ts'
 import userRoutes from './routes/users.ts'
+
+if (process.env.NODE_ENV !== 'production') {
+  import('dotenv')
+    .then((dotenv) => dotenv.config())
+    .catch((err) => {
+      console.error('Failed to load dotenv: ', err)
+    })
+}
 
 const __filename = URL.fileURLToPath(import.meta.url)
 const __dirname = Path.dirname(__filename)
