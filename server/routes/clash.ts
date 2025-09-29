2c2e2db4e0224ae20feffadc1caccf0e920f49b5
Abe <214738266+abe-calder@users.noreply.github.com>
2025-09-29 18:12:34 +1300

    change

--- server/routes/clash.ts
+++ server/routes/clash.ts
@@ -14,7 +14,7 @@ router.get('/', checkJwt, async (req: JwtRequest, res) => {
       .get(`https://api.clashroyale.com/v1/players/%23${user.playerTag}`)
       .set(
         'Authorization',
-        `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjFiN2ZiNzgzLTQyM2ItNGFmNy1iMWExLWI5ZmM1NGQzMmYxNyIsImlhdCI6MTc1NDg5ODM1Nywic3ViIjoiZGV2ZWxvcGVyLzY1YWNjYzVjLTE0YzUtNTA2Ny0xZDY0LTczMTVkYTBiZjI4NSIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIyNy4yNTIuODguMTk1Il0sInR5cGUiOiJjbGllbnQifV19.HdMeURFKEJmv14nLVhfS5P6Z73XqkZfwhxsRYNc6N6BGfgbvGxx7O25cYOmGD7KndALQteoXRDPmyEFU7kRorg`,
+        `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjhmMWRlNzM0LTY1ODktNDA2MS04NDk2LTYwZGE0M2Q0NGMyNCIsImlhdCI6MTc1OTEyMjcxNiwic3ViIjoiZGV2ZWxvcGVyLzY1YWNjYzVjLTE0YzUtNTA2Ny0xZDY0LTczMTVkYTBiZjI4NSIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI1NC4xODguNzEuOTQiXSwidHlwZSI6ImNsaWVudCJ9XX0.vbLxodxvhhx7qpImIuzsN-q4bz-KCQW6MhlfcJJGkR_jz0FvrDB312RilpWp8rPVfEYvCrDuxmF26RdJf53onA`,
       )
     res.json(response.body)
   } catch (error) {
