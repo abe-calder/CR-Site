145f8191e02149573b2278e6e5efad3adbef3fc1
Abe <214738266+abe-calder@users.noreply.github.com>
2025-09-29 18:08:03 +1300

    fix

--- server/routes/clashBattlelog.ts
+++ server/routes/clashBattlelog.ts
@@ -1,19 +1,10 @@
 import express from 'express'
 import request from 'superagent'
-if (process.env.NODE_ENV !== 'production') {
-  import('dotenv/')
-    .then((dotenv) => dotenv.config())
-    .catch((err) => {
-      console.error('Failed to load dotenv: ', err)
-    })
-}
 import checkJwt from '../auth0'
 import { JwtRequest } from '../auth0'
 import { getUserById } from '../db/functions/users'
 
 const router2 = express.Router()
-
-const CR_API_TOKEN = process.env.CR_API_TOKEN
 
 router2.get('/', checkJwt, async (req: JwtRequest, res) => {
   try {
@@ -23,7 +14,10 @@ router2.get('/', checkJwt, async (req: JwtRequest, res) => {
       .get(
         `https://api.clashroyale.com/v1/players/%23${user.playerTag}/battlelog`,
       )
-      .set('Authorization', `Bearer ${CR_API_TOKEN}`)
+      .set(
+        'Authorization',
+        `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjFiN2ZiNzgzLTQyM2ItNGFmNy1iMWExLWI5ZmM1NGQzMmYxNyIsImlhdCI6MTc1NDg5ODM1Nywic3ViIjoiZGV2ZWxvcGVyLzY1YWNjYzVjLTE0YzUtNTA2Ny0xZDY0LTczMTVkYTBiZjI4NSIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIyNy4yNTIuODguMTk1Il0sInR5cGUiOiJjbGllbnQifV19.HdMeURFKEJmv14nLVhfS5P6Z73XqkZfwhxsRYNc6N6BGfgbvGxx7O25cYOmGD7KndALQteoXRDPmyEFU7kRorg`,
+      )
     res.json(response.body)
   } catch (error) {
     console.error(error)
